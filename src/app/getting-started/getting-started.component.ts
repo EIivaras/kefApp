ecd1293d37acc0787d6a31d7daa229ac3fcaaca9
EIivaras <zach00182@gmail.com>
2017-11-14 21:09:09 -0500

    Mat-card'ing everything!

--- src/app/getting-started/getting-started.component.ts
+++ src/app/getting-started/getting-started.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'kef-getting-started',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GettingStartedComponent implements OnInit {
 
-  constructor() { }
+  constructor( private _router: Router ) { }
 
   ngOnInit() {
   }
 
+  rulesClick() {
+    this._router.navigateByUrl('/rules');
+  }
+
+  faqClick() {
+    this._router.navigateByUrl('/faq');
+  }
+
+  resourcesClick() {
+    this._router.navigateByUrl('/resources');
+  }
+
+  articlesClick() {
+    this._router.navigateByUrl('/introduction-to-roleplay');
+  }
 }
