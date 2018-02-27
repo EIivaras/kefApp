cf1371ee6540bb68be1a96edce887cf412fe4c70
Zach Swearingen <zach.swearingen@gmail.com>
2018-02-26 20:25:40 -0600

    Exploratory changes to navigation structure and layout.

--- src/app/creating-your-character/creating-your-character.component.ts
+++ src/app/creating-your-character/creating-your-character.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-creating-your-character',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatingYourCharacterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router : Router) { }
 
   ngOnInit() {
   }
 
+  backToRoot() {
+    this._router.navigateByUrl('/articles');
+  }
 }
--- src/app/introduction-to-roleplay/introduction-to-roleplay.component.ts
+++ src/app/introduction-to-roleplay/introduction-to-roleplay.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-introduction-to-roleplay',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroductionToRoleplayComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router : Router) { }
 
   ngOnInit() {
+  }
+
+  backToRoot() {
+    this._router.navigateByUrl('/articles');
   }
 
 }
